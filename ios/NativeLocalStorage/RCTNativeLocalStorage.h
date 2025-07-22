041512fe4657a4c2227f5eb506e18b26adf0afe6
Aditya Singh <aditya.kumar@xenelsoft.co.in>
2025-07-23 00:09:30 +0530

    local storage native module integrated

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,19 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by macos on 22/07/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+
+@end
+
+NS_ASSUME_NONNULL_END
